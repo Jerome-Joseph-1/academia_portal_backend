a25133e28bc379c2a94ba5a9f4ce36bb03ca1f2f
Jerome Joseph <jeromejoseph5813@gmail.com>
2024-11-28 03:58:28 +0530

    feat: initialize project structure

---
+++ src/main/java/com/academia/arcademia_backend/ArcademiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.academia.arcademia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcademiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcademiaBackendApplication.class, args);
+    }
+
+}
